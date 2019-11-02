afb0be54300686ecc69c6f9d505732c355a06577
vasylw <vasylw@gmail.com>
2019-11-02 19:35:34 +0200

    jklkgklgbhkku,hbmhgljh

--- Database/Database.tf
+++ Database/Database.tf
@@ -31,7 +31,7 @@ provisioner "local-exec" {
   }
 
 provisioner "local-exec" {
-    command = "echo -e [database_server] \n${aws_instance.database.private_ip}" > database_server"
+    command = "echo -e [database_server] \n${aws_instance.database.private_ip} > database_server"
   }
 
 }

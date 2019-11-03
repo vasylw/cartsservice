5edbbade705a8f55d084f9b1b239f2df45830ba1
vasylw <vasylw@gmail.com>
2019-11-03 20:17:25 +0200

    Terraform Provisioner local-exec interpreter arguments2

--- Appserver/Appserver.tf
+++ Appserver/Appserver.tf
@@ -35,7 +35,7 @@ provisioner "local-exec" {
   }
 
 provisioner "local-exec" {
-    command = "echo '[carts_app_server] \n${aws_instance.carts_app.private_ip}' > carts_app_server"
+    command = "echo [carts_app_server] \n${aws_instance.carts_app.private_ip} > carts_app_server"
     interpreter = ["/bin/bash", "-e"]
   }
 

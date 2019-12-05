84c1e2e4321412d78a2ea6426fef3a3889219548
Vasylw <vasylw@gmail.com>
2019-12-05 17:48:13 +0200

    Update Cartsservice.tf

--- Cartsservice.tf
+++ Cartsservice.tf
@@ -148,6 +148,101 @@ resource "aws_security_group" "database_sg" {
   }
 }
 
+# Create AWS security group for ELK_server
+resource "aws_security_group" "elk_sg" {
+  name        = "elk_sg"
+  description = "Allow port in/out_bound traffic"
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  
+  egress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   # Allow all in/out_bound
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.subnet]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.subnet]
+  }
+
+  # Enable ICMP
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  
+  tags = {
+    Name = "allow_all_elk"
+  }
+}
 
 # Create a AWS instance for Application server, using module
 module "carts_app" {
@@ -170,3 +265,12 @@ module "database" {
   sg_db_id = aws_security_group.database_sg.id
 }
 
+  # Create a AWS instance for database server, using module
+module "elk" {
+  source      = "./ELK"
+  availability_zone     = var.azone[var.uf_azone]
+  image_id = var.amis[var.region[var.uf_region]]
+  instance_name = "elk"
+  instance_type = var.instance_type
+  sg_elk_id = aws_security_group.elk_sg.id
+}
